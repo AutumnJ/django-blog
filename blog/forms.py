bd6ff2d9973240a1e9930e5d60f0d5cc9f79129b
Autumn Kangas <autumnkangas@gmail.com>
2018-03-03 14:58:17 -0600

    added edit and create functionalties

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+  #PostForm is the name of our form
+  # Tell Django that this form is a ModelForm – forms.ModelForm is responsible for that
+
+    class Meta:
+      #class Meta, where we tell Django which model should be used to create this form (model = Post)
+        model = Post
+        fields = ('title', 'text',)
+        #only title & text fields are exposed
+        #author is automatically assigned (logged in user)
+        #created_date is set when post is created
